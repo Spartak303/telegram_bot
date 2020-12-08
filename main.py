8b58d734c08b2f6b2c256a3e8de4783154154564
datsko303 <datsko303@gmail.com>
2020-12-08 12:37:14 +0200

    оброботчик команди - help

--- main.py
+++ main.py
@@ -28,4 +28,10 @@ def send_welcome(message):
     with open('users.json', 'w', encoding="utf-8") as js:
         json.dump(users_data, js)
 
-
+@bot.message_handler(commands=["help"])
+def show_commands(message):
+    bot.send_message(message.chat.id, "/start - Початок роботи"
+                                      "\n/set_time - Встановити час"
+                                      "\n/set_role - Встановити роль"
+                                      "\n/help - Показати доступні команди"
+                                      "\n/leave - Вимкнути сповіщення")
