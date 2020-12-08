251a07ba0fb45b20b8aec4516fc7c0f652425d20
datsko303 <datsko303@gmail.com>
2020-12-08 12:17:27 +0200

    initial commit + @bot.message hendler

---
+++ main.py
@@ -0,0 +1,31 @@
+import json
+import telebot
+import config
+import json
+import threading
+from datetime import *
+import time
+import logging
+
+bot = telebot.TeleBot(config.TOKEN)
+
+with open("users.json", encoding="utf-8") as js:
+    users_data = json.load(js)
+
+
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+    bot.send_message(message.chat.id, f"Вітаю {message.from_user.first_name}!\n"
+                                      f"Я <b>{bot.get_me().first_name}</b>,"
+                                      f" бот який сповіщує працівника про початок роботи. "
+                                      f"\nНапиши /set_time щоб встановити час сповіщень"
+                                      f"\nНапиши /set_role щоб встановити свою роль в компанії"
+                                      f"\nНапиши /help для перегляду доступних команд.",
+                                      parse_mode='html')
+
+    users_data[f"{message.chat.id}"] = {"time": "", "role": ""}
+
+    with open('users.json', 'w', encoding="utf-8") as js:
+        json.dump(users_data, js)
+
+
